4e1ea7de85fa9c49cc1a9c2cee446889f27c1ac6
Rajesh Dewle <rajesh_dewle@hotmail.com>
2020-12-28 12:37:12 +0530

    ⚡ Improve home page styles

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -4,26 +4,18 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    <div class="flex min-h-screen">
+    <div class="flex min-h-screen justify-between">
         <div class="w-1/6 border-r border-gray-400">
             @include('layouts.sidebar-links')
         </div>
         <div class="flex-1">
             <h1 class="p-4 font-bold text-lg">Home</h1>
-            <div class="border-t border-b border-gray-400 p-2">
-                <form action="/">
-                    <textarea name="body" cols="30" rows="5" placeholder="What's happening?"
-                        class="w-full border-0 resize-none shadow-none"></textarea>
-
-                    <div class="flex justify-end">
-                        <button type="submit"
-                            class="rounded-full bg-blue-400 text-white font-bold px-3 py-2">Tweet</button>
-                    </div>
-                </form>
-            </div>
-            <div></div>
+            <x-tweet-form />
+            @foreach ($tweets as $tweet)
+            <x-tweet :tweet="$tweet" />
+            @endforeach
         </div>
-        <div class="w-1/4 border-l border-gray-400 p-4">
+        <div class=" w-1/4 border-l border-gray-400 p-4">
             @include('layouts.friends')
         </div>
     </div>
--- resources/views/layouts/friends.blade.php
+++ resources/views/layouts/friends.blade.php
@@ -1,7 +1,7 @@
 <h3 class="font-bold text-lg">Who to follow</h3>
 
 <ul>
-    <li class="mb-4">
+    <li class="mb-4 border-b border-gray-400 py-4">
         <div class="flex items-center">
             <img src="https://i.pravatar.cc/40" alt="" class="rounded-full mr-2">
             John Doe
